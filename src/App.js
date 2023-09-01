7ac7e747c643e83c62b1afe519b54fd16d20e611
Christian Flores <christianflores@Christians-MacBook-Air.local>
2023-09-01 12:37:39 -0700

    tagline

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>Browser Parking Lot</h1>
+          <h1>Browser Parking Lot</h1>
+          <p>Send most of your browser tabs into retirement.</p>
       </header>
     </div>
   );
